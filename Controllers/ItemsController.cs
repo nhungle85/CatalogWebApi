8b2ec2c66afc889ea5eb2ea959657cb62dbe109f
misssg85 <misssg85@gmail.com>
2021-12-03 14:36:30 -0800

    Item entity and controller

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,42 @@
+﻿using CatalogWebApi.Entities;
+using CatalogWebApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogWebApi.Controllers
+{
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemRepo repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemRepo();
+        }
+
+        //GET /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        //GET /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            if (item is null) {
+                return NotFound();
+            }
+            return item;
+        }
+    }
+}
